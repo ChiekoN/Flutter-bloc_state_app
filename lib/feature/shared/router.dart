9c7d5aac617ff00329f042aa0911016b5e65e748
Chieko Natori <chieko.natori@gmail.com>
2025-04-02 15:42:28 +0800

    First commit for bloc_state_app. Separate to bottom_navbar.dart, change src/ to feature/

---
+++ lib/feature/shared/router.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../pages/toplist/toplist.dart';
+import '../pages/add/add.dart';
+import '../pages/detail/detail.dart';
+import '../pages/history/history.dart';
+import 'bottom_navbar.dart';
+
+
+class AppRoutes {
+  static const home = '/';
+  static const add = 'add';
+  static const detail = 'detail';
+  static const history = '/history';
+}
+
+const List<NavigationDestinationRoute> destinations = [
+  NavigationDestinationRoute(
+    route: AppRoutes.home,
+    label: 'Home',
+    icon: Icon(Icons.home),
+  ),
+  NavigationDestinationRoute(
+    route: AppRoutes.history,
+    label: 'History',
+    icon: Icon(Icons.history),
+  ),
+];
+
+class NavigationDestinationRoute {
+  const NavigationDestinationRoute({
+    required this.route,
+    required this.label,
+    required this.icon,
+  });
+
+  final String route;
+  final String label;
+  final Icon icon;
+}
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: AppRoutes.home,
+      pageBuilder: (context, state) => NoTransitionPage(
+        child: ScaffoldWithBottomNavBar(
+          currentIndex: 0,
+          child: TopListPage(),
+        ),
+      ),
+      routes: [
+        GoRoute(
+          path: AppRoutes.add,
+          pageBuilder: (context, state) => NoTransitionPage(
+            child: NewDatePage(),
+          ),
+        ),
+        GoRoute(
+          path: '${AppRoutes.detail}/:index',
+          pageBuilder: (context, state) => NoTransitionPage(
+            child: DateDetailPage(
+              index: state.pathParameters['index']
+            ),
+          ),
+        ),
+      ],
+    ),
+    GoRoute(
+      path: AppRoutes.history,
+      pageBuilder: (context, state) => NoTransitionPage(
+        child: ScaffoldWithBottomNavBar(
+          currentIndex: 1, // Should match with destinations[N].index for '/history'
+          child: HistoryListPage(),
+        ),
+      ),
+    ),
+  ],
+);
+
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:universal_platform/universal_platform.dart';
 
-import 'src/shared/app.dart';
+import 'feature/shared/app.dart';
 
 Future setDesktopWindow() async {
   await DesktopWindow.setWindowSize(const Size(412, 983));
