63e72a1ce7047eee0e9b61243a6db56bf411bdc9
Chieko Natori <chieko.natori@gmail.com>
2025-04-03 17:33:19 +0800

    Create Add page (SelectedDateString not show up yet)

--- lib/feature/pages/add/add.dart
+++ lib/feature/pages/add/add.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../shared/router.dart';
-
-
+import '../../../domain/models/todate.dart';
 
 class NewDatePage extends StatefulWidget {
   const NewDatePage({super.key});
@@ -14,18 +13,109 @@ class NewDatePage extends StatefulWidget {
 
 class _NewDatePageState extends State<NewDatePage> {
 
+  DateTime firstDate = DateTime.now().add(Duration(days:1)); // Available date is tomorrow and after.
+  DateTime selectedDate = DateTime.now().add(Duration(days:1)); // Initial value
+  DateTime lastDate = DateTime(2200); // Max date
+  String selectedDateString = '';
+
+  final textContoroller = TextEditingController();
+  String title = '';
+
+  final memoController = TextEditingController();
+  String? memo;
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textContoroller.dispose();
+    memoController.dispose();
+    super.dispose();
+  }
+
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: firstDate,
+      lastDate: lastDate,
+    );
+    print(picked);
+
+    if (picked != null && picked != selectedDate){
+      setState(() {
+        selectedDate = picked;
+        selectedDateString = '${selectedDate.toLocal()}'.split(' ')[0];
+      });
+      print('selectedDateString = $selectedDateString'); // DEV
+    }
+  }
+
+  
+  String?  onSave() {
+    // If selectedDateString is empty OR title is empty, 
+    //    return null;
+    // Else
+    //    todateRepo.addTodate(newTodate);
+    return null;
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is a page to add new date.',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(8, 16, 8, 16),
+        child: 
+          Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Add your important day!',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Title',
+                ),
+                controller: textContoroller,
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _selectDate(context),
+                    child: const Text('Date'),
+                  ),
+                  Text(
+                    selectedDateString,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  )
+                ],
+              ),
+              TextField(
+                maxLength: 2000,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Add some notes (optional)',
+                  labelText: 'Memo',
+                ),
+                controller: memoController,
+              ),
+              // Save button
+              FilledButton(
+                onPressed: null,
+                child: Text('Save'),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
